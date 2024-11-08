8beec8bbaa2c53045a56f9ba2bc9ae3affd425a6
Andreas Nordahl <andnorda@gmail.com>
2024-11-08 09:12:04 +0100

    github text

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -32,12 +32,21 @@
 		</ul>
 	</nav>
 	<!-- <div class="logo"/> -->
-	<a class="nav-link" href="https://github.com/nais"
-		><img class="github" src="github-mark.svg" alt="nais github" /></a
-	>
+	<a class="github-link nav-link" href="https://github.com/nais"
+		><img class="github" src="github-mark.svg" alt="nais github" />95% åpen kildekode
+	</a>
 </div>
 
 <style>
+	.github {
+		height: 24px;
+	}
+	.nav-link.github-link {
+		text-transform: none;
+		display: flex;
+		align-items: center;
+		gap: 8px;
+	}
 	@import url('https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i');
 	.row {
 		font-size: 14px;
@@ -115,8 +124,5 @@
 		align-items: center;
 		align-content: center;
 		justify-content: space-between;
-	}
-	.github {
-		height: 32px;
 	}
 </style>
