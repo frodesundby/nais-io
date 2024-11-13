794b2a4ab166d08c0bf3613f6afc9813f1795b25
Andreas Nordahl <andnorda@gmail.com>
2024-11-13 13:19:05 +0100

    add amplitude

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
 	import Header from '$lib/Header.svelte';
 	import '../styles/app.css';
+
+	import amplitude from '@amplitude/analytics-browser';
+
+	const getApiKey = () => {
+		if (window.location.href.includes('nais.io')) {
+			return '16d1ee2fd894ca2562eeebb5095dbcf0'; // prod
+		}
+		return '04203d48401492bda4620a74acf85a5b'; // dev
+	};
+
+	export const loggInstance = amplitude.init(getApiKey(), {
+		serverUrl: 'amplitude.nav.no/collect',
+		serverZone: 'EU'
+	});
 </script>
 
 <Header />
